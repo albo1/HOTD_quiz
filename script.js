4d925337c869e4e91cabc9c021cdcf205c8e596c
alboy77 <al.boucetta@yahoo.com>
2023-03-27 17:40:15 -0400

    multiple chioice variables add

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ var introContainer = document.getElementById("welcome");
 var quizContainer = document.getElementById("quiz-container");
 var mainSubmitBtn = document.getElementById("submit");
 var timeEl = document.getElementById("timer");
-var quizQuestion = document.getElementById("choice-question"):
+var quizQuestion = document.getElementById("choice-question");
+var answerA = document.getElementById("choice-a");
+var answerB = document.getElementById("choice-b");
+var answerC = document.getElementById("choice-c");
+var answerD = document.getElementById("choice-d");
+var gameOverContainer = document.getElementById("end-game");
+
