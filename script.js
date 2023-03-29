cc61800d7108c638120cad640cd62a25d3532e3e
alboy77 <al.boucetta@yahoo.com>
2023-03-28 20:30:16 -0400

    tried the best i could

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ var timeLeft;
 var objQuestion;
 var correctAnswer;
 var currentQuestionIndex = 0;
+var highScores = JSON.parse(localStorage.getItem("highScores"))
+var highScore = localStorage.getItem('highScore');
 
 var questions = [{
     q: "What is the name of Rhaenyra's Dragon?",
@@ -61,59 +63,59 @@ mainSubmitBtn.addEventListener("click", function () {
 
 function setQuestions(i) {
     objQuestion = questions[i];
-  
+
     if (!objQuestion) {
-      gameDone();
-      return;
+        gameDone();
+        return;
     }
-  
+
     correctAnswer = objQuestion.a;
     const button1 = document.createElement("button");
     const button2 = document.createElement("button");
     const button3 = document.createElement("button");
     const button4 = document.createElement("button");
-  
+
     quizQuestion.textContent = objQuestion.q;
     button1.textContent = objQuestion.c1;
     button2.textContent = objQuestion.c2;
     button3.textContent = objQuestion.c3;
     button4.textContent = objQuestion.c4;
-  
+
     document.querySelector(".buttons").innerHTML = "";
     document.querySelector(".buttons").append(button1);
     document.querySelector(".buttons").append(button2);
     document.querySelector(".buttons").append(button3);
     document.querySelector(".buttons").append(button4);
-  
+
     // Remove previous event listener before adding a new one
     document
-      .querySelector(".buttons")
-      .removeEventListener("click", buttonClickHandler);
+        .querySelector(".buttons")
+        .removeEventListener("click", buttonClickHandler);
     document
-      .querySelector(".buttons")
-      .addEventListener("click", buttonClickHandler);
-  
+        .querySelector(".buttons")
+        .addEventListener("click", buttonClickHandler);
+
     function buttonClickHandler(event) {
-      if (event.target.matches("button")) {
-        if (event.target.textContent === correctAnswer) {
-          console.log("yay");
-          setQuestions(i + 1);
-        } else {
-          console.log("boo");
-          timeLeft -= 2;
-          if (timeLeft <= 0) {
-            timeLeft = 0;
-            gameDone();
-          } else {
-            setQuestions(i + 1);
-          }
+        if (event.target.matches("button")) {
+            if (event.target.textContent === correctAnswer) {
+                console.log("yay");
+                setQuestions(i + 1);
+            } else {
+                console.log("boo");
+                timeLeft -= 5;
+                if (timeLeft <= 0) {
+                    timeLeft = 0;
+                    gameDone();
+                } else {
+                    setQuestions(i + 1);
+                }
+            }
+            event.stopImmediatePropagation();
+            currentQuestionIndex++;
+            setQuestions(currentQuestionIndex);
         }
-        event.stopImmediatePropagation();
-        currentQuestionIndex++;
-        setQuestions(currentQuestionIndex);
-      }
     }
-  }
+}
 
 function checkCorrectAnswer(chosenAnswer) {
     if (chosenAnswer === correctAnswer) {
@@ -139,8 +141,14 @@ function gameDone() {
     gameOverContainer.style.display = "block";
     var finalScore = timeLeft;
     var gameOverMessage = "YOU DIED! Your final score is: " + finalScore;
+    gameOverMessage += "<br>High Score: " + highScore;
     gameOverContainer.textContent = gameOverMessage;
-    // setQuestions(questions.length -1)
+    if (finalScore > highScore) {
+        localStorage.setItem('highScore', finalScore);
+        var saveBtn = document.getElementById("saveBtn");
+        
+    }
 }
+
 
 setQuestions(0);
